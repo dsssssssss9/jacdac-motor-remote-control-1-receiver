c8a32cfe0c0301fa59e82464a9b28a34773ebda6
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 22:18:43 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,6 +17,11 @@ input.onButtonPressed(Button.B, function () {
     robotbit.MotorStopAll()
     robotbit.MotorRun(robotbit.Motors.M1A, -144)
 })
+radio.onReceivedValue(function (name, value) {
+    Speed = value
+    basic.showNumber(Speed)
+})
+let Speed = 0
 let String2 = ""
 let Number2 = 0
 Number2 = 0
