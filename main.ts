f63c9244f37edf8da27981533512410c3b621fd4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 21:43:18 +0100

    added button control test

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, 255)
+})
+input.onButtonPressed(Button.AB, function () {
+    robotbit.MotorStopAll()
+})
+input.onButtonPressed(Button.B, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, -144)
+})
 robotbit.MotorStopAll()
 radio.setGroup(42)
 let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
