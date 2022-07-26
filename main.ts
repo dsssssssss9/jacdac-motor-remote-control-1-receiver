dc267362e34abe21be183aba92d1a6ca91172108
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 23:28:50 +0100

    add comments

--- main.ts
+++ main.ts
@@ -1,28 +1,32 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    Number2 = receivedNumber
-})
-input.onButtonPressed(Button.AB, function () {
-    robotbit.MotorStopAll()
-})
-input.onButtonPressed(Button.B, function () {
-    robotbit.MotorStopAll()
-    robotbit.MotorRun(robotbit.Motors.M1A, -144)
-})
-radio.onReceivedString(function (receivedString) {
-    String2 = receivedString
-    basic.showString(String2)
-})
+/**
+ * Jacdac Motor Remote Control Receiver
+ * 
+ * This code uses a KittenBot RobotBit to control the speed & direction of a 3v DC motor connected to it 
+ * 
+ * Speed & direction are derived from values sent from a transmitter Micro:Bit
+ */
+// If received data = "Name" then set variable "Speed" to value of data 
+// In this case the slider has value range  from 0 to 100 so this is mapped to 0 - 255 as that is whet the RobotBit Motor value range is  
 radio.onReceivedValue(function (name, value) {
     if (name == "Speed") {
         Speed = Math.map(value, 0, 100, 0, 255)
     }
 })
-input.onButtonPressed(Button.A, function () {
-    robotbit.MotorStopAll()
-    robotbit.MotorRun(robotbit.Motors.M1A, 255)
+// Set Variable "Number" to number received from Transmitter
+radio.onReceivedNumber(function (receivedNumber) {
+    Number2 = receivedNumber
 })
+// on start do following...
+// 
+// set motor speed to 0
+// stop motor
+// 
+// Set Radio Group
+// 
+// Initialise RobotBit On-Board Neopixels ( Pin16)
+// 
+// Display Tick on Micro:Bit & Flash Neopixels green to signify receiver is ready 
 let Speed = 0
-let String2 = ""
 let Number2 = 0
 Number2 = 0
 robotbit.MotorStopAll()
@@ -34,6 +38,7 @@ strip.show()
 basic.pause(200)
 strip.clear()
 strip.show()
+// set motor to rotate in direction based on number ( 1 is clockwise , -1 is anti-clockwise ) at speed determined by value received from transmitter. In this case a Slide Potentiometer
 basic.forever(function () {
     if (Number2 == 1) {
         robotbit.MotorRun(robotbit.Motors.M1A, Speed)
